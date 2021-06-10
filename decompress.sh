etmula/mmlfiles

Create decompress.sh

decompress.sh
@@ -1,0 +1,11 @@
+echo Decompressing abstr/
+tar -I pbzip2 -xf abstr.tar.gz
+
+echo Decompressing fmbibs/
+tar -I pbzip2 -xf fmbibs.tar.gz
+
+echo Decompressing html/
+tar -I pbzip2 -xf html.tar.gz
+
+echo Decompressing mml/
+tar -I pbzip2 -xf mml.tar.gz

mimosa-project/emwiki-mmlfiles

decompress.sh
@@ -1,0 +1,11 @@
+echo Decompressing abstr/
+tar -I pbzip2 -xf abstr.tar.bz2
+
+echo Decompressing fmbibs/
+tar -I pbzip2 -xf fmbibs.tar.bz2
+
+echo Decompressing html/
+tar -I pbzip2 -xf html.tar.bz2
+
+echo Decompressing mml/
+tar -I pbzip2 -xf mml.tar.bz2