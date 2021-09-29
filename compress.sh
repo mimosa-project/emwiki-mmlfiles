f8b3872679c28b274073ab60b17cb43f65f92adb
etmula <mountainroad@hotmail.co.jp>
2021-09-29 17:49:42 +0000

    Add article, search, symbol files
    Add Htmlized MML files in article.tar.bz2
    Add index files in search.tar.bz2
    Add MML Reference HTML files in symbol.tar.bz2
    Add article, search, symbol to compress.sh & decompress.sh &.gitignore

--- compress.sh
+++ compress.sh
@@ -9,3 +9,12 @@ tar -I pbzip2 -cf html.tar.bz2 html
 
 echo Compressing mml/
 tar -I pbzip2 -cf mml.tar.bz2 mml
+
+echo Compressing article/
+tar -I pbzip2 -cf article.tar.bz2 article
+
+echo Compressing symbol/
+tar -I pbzip2 -cf symbol.tar.bz2 symbol
+
+echo Compressing search/
+find search/index/ -type f ! -name "*.tar.bz2" | xargs -i tar --use-compress-program=pbzip2 -cf {}.tar.bz2 {}
